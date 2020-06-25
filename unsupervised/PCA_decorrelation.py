725d4ec513822e8f1889f47f4ca2607f78db2712
Suchana Chakrabarti <suchanachakraborty@gmail.com>
2020-06-26 03:09:20 +0530

    Update PCA_decorrelation.py

--- unsupervised/PCA_decorrelation.py
+++ unsupervised/PCA_decorrelation.py
@@ -26,3 +26,26 @@ correlation, pvalue = pearsonr(xs, ys)
 print(correlation)
 
 #OUTPUT - 2.54e-17
+
+# plotting the arrow in the direction of maxm variance
+# or the first_component
+# Make a scatter plot of the untransformed points
+plt.scatter(grains[:,0], grains[:,1])
+
+# Create a PCA instance: model
+model = PCA()
+# Fit model to points
+model.fit(grains)
+
+# Get the mean of the grain samples: mean
+mean = model.mean_
+
+# Get the first principal component: first_pc
+first_pc = model.components_[0,:]
+
+# Plot first_pc as an arrow, starting at mean
+plt.arrow(mean[0], mean[1], first_pc[0], first_pc[1], color='red', width=0.01)
+
+# Keep axes on same scale
+plt.axis('equal')
+plt.show()
