7cc379ea8bff908c9b8462720e41b5a97030d5d1
suchana <suchanachakraborty@gmail.com>
2020-10-19 20:47:21 +0530

    varios boosting techniques

---
+++ Tree_Models/boosting/gradient_boosting.py
@@ -0,0 +1,25 @@
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate gb
+gb = GradientBoostingRegressor(max_depth=4, 
+            n_estimators=200,
+            random_state=2)
+
+# Fit gb to the training set
+gb.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = gb.predict(X_test)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute MSE
+mse_test = MSE(y_test, y_pred)
+
+# Compute RMSE
+rmse_test = mse_test**(1/2)
+
+# Print RMSE
+print('Test set RMSE of gb: {:.3f}'.format(rmse_test))
---
+++ Tree_Models/boosting/stochastic_gradient_booster.py
@@ -0,0 +1,27 @@
+# Import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Instantiate sgbr
+sgbr = GradientBoostingRegressor(max_depth=4, 
+            subsample=0.9,
+            max_features=0.75,
+            n_estimators=200,                                
+            random_state=2)
+
+# Fit sgbr to the training set
+sgbr.fit(X_train, y_train)
+
+# Predict test set labels
+y_pred = sgbr.predict(X_test)
+
+# Import mean_squared_error as MSE
+from sklearn.metrics import mean_squared_error as MSE
+
+# Compute test set MSE
+mse_test = MSE(y_test, y_pred)
+
+# Compute test set RMSE
+rmse_test = mse_test**(1/2)
+
+# Print rmse_test
+print('Test set RMSE of sgbr: {:.3f}'.format(rmse_test))
